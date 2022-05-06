e5167a17dd6faa6ca946932785605d0277e0d10a
Morales <namormol@gmail.com>
2022-05-06 10:44:29 +0200

    Modifico la clae Almacen

--- src/main/java/es/electromarket/Almacen.java
+++ src/main/java/es/electromarket/Almacen.java
@@ -4,26 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Almacen {
-	
-	private List<Electrodomestico> stock = new ArrayList<Electrodomestico>();
 
-	
-	public List<Electrodomestico> getStock() {
-		return stock;
-	}
+  private String localidad;
+  private List<Electrodomestico> stock = new ArrayList<Electrodomestico>();
 
-	
-	public void setStock(List<Electrodomestico> stock) {
-		this.stock = stock;
-	}
 
-	public Almacen() {
-		
-	}
+  public String getLocalidad() {
+    return localidad;
+  }
 
-	public Almacen(List<Electrodomestico> stock) {
-		super();
-		this.stock = stock;
-	}
-	
+  public void setLocalidad(String localidad) {
+    this.localidad = localidad;
+  }
+
+  public List<Electrodomestico> getStock() {
+    return stock;
+  }
+
+
+  public void setStock(List<Electrodomestico> stock) {
+    this.stock = stock;
+  }
+
+  public Almacen() {
+
+  }
+
+  public Almacen(String localidad, List<Electrodomestico> stock) {
+    this.localidad = localidad;
+    this.stock = stock;
+  }
+
 }
